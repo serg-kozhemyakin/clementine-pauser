543aef00f4ffcd01c703a5d7c0fc27c0c6c47355
Serg Kozhemyakin <serg.kozhemyakin@gmail.com>
2013-04-03 16:36:39 +0300

    resume playback only if paused by pauser

--- clementine-pauser.scm
+++ clementine-pauser.scm
@@ -12,18 +12,35 @@
                             path: '/Player
                             interface: 'org.freedesktop.MediaPlayer ))
 
+(define prop-context (dbus:make-context
+                      service: 'org.mpris.clementine
+                      path: '/org/mpris/MediaPlayer2
+                      interface: 'org.freedesktop.DBus.Properties ))
+
+(define paused-by-pauser #f)
+
+(define (clementine-playing?)
+  (string=? "Playing"
+            (variant-data
+             (car (dbus:call prop-context "Get" "org.mpris.MediaPlayer2.Player" "PlaybackStatus")))))
+
 (define (control-clementine state)
   (cond
-    [(eq? state 'play) (dbus:call clementine-context "Play")]
-    [(eq? state 'pause) (dbus:call clementine-context "Pause")]))
+    [(eq? state 'play)
+     (if paused-by-pauser
+         (dbus:call clementine-context "Play"))]
+    [(eq? state 'pause)
+     (if (set! paused-by-pauser (clementine-playing?))
+           (dbus:call clementine-context "Pause"))]
+    ))
 
 (define (play-pause state)
   (if state (control-clementine 'pause)
       (control-clementine 'play)))
 
-(dbus:register-signal-handler screensaver-context 'ActiveChanged play-pause)
-
 (dbus:enable-polling-thread! enable: #f)
+
+(dbus:register-signal-handler screensaver-context 'ActiveChanged play-pause)
 
 (let loop ()
   (dbus:poll-for-message )
